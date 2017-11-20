2e6191be365842b3c8d0e67f06bb3a0292718c1b
Maksim Parkhomenko <perolwy@gmail.com>
2017-11-20 18:51:23 +0300

    u-transnet node is default in settings

--- app/api/apiConfig.js
+++ app/api/apiConfig.js
@@ -20,19 +20,10 @@ export const settingsAPIs = {
     WS_NODE_LIST: [
         {url: "wss://fake.automatic-selection.com", location: {translate: "settings.api_closest"}},
         {url: "ws://127.0.0.1:8090", location: "Locally hosted"},
-        {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
-        {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
-        {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
-        {url: "wss://bts.ai.la/ws", location: "Hong Kong"},
-        {url: "wss://bitshares.apasia.tech/ws", location: "Bangkok, Thailand"},
-        {url: "wss://bitshares.dacplay.org/ws", location:  "Hangzhou, China"},
-        {url: "wss://bitshares-api.wancloud.io/ws", location:  "China"},
-        {url: "wss://openledger.hk/ws", location: "Hong Kong"},
-        {url: "wss://dexnode.net/ws", location: "Dallas, USA"},
-        {url: "wss://bitshares.crypto.fans/ws", location: "Munich, Germany"},
+        {url: "ws://95.85.60.154:11011", location: "utransnet"},
         {url: "wss://node.testnet.bitshares.eu", location: "Public Testnet Server (Frankfurt, Germany)"}
     ],
-    DEFAULT_FAUCET: "https://bitshares.openledger.info",
+    DEFAULT_FAUCET: "http://95.85.60.154",
     TESTNET_FAUCET: "https://faucet.testnet.bitshares.eu",
     RPC_URL: "https://openledger.info/api/"
 };
